7548e6f729b7bd0fc4a9b3cc5fb9c7da18f4fabd
Koutah15 <kiruka1212@gmail.com>
2025-03-03 02:40:18 +0900

    page変更

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -7,14 +7,14 @@ import ReactMarkdown from "react-markdown";
 import { useParams } from "next/navigation";
 import { useDocumentContext, Document } from "@/context/DocumentContext";
 
-  // `id` に基づいて取得するカラムを決定
-  const columnMap: Record<string, keyof Document> = {
-    "privacy-policy": "privacy_policy",
-    "terms-of-service": "terms_of_service",
-    "community-guidelines": "community_guidelines",
-    "maintenance-status": "maintenance_status",
-    "patch-notes": "patch_notes",
-  };
+// `id` に基づいて取得するカラムを決定
+const columnMap: Record<string, keyof Document> = {
+  "privacy-policy": "privacy_policy",
+  "terms-of-service": "terms_of_service",
+  "community-guidelines": "community_guidelines",
+  "maintenance-status": "maintenance_status",
+  "patch-notes": "patch_notes",
+};
 
 export default function DocumentPage() {
   const params = useParams();
@@ -48,22 +48,13 @@ export default function DocumentPage() {
   }
 
   return (
-    <div className="flex-1 flex items-center justify-center px-4 md:px-8 ml-0 lg:ml-0 sm:ml-64">
+    <div className="flex-1 flex items-center justify-center px-4 md:px-8 ml-0 lg:ml-0 sm:ml-64 mt-12">
       <div className="flex flex-col items-center w-full max-w-2xl h-screen">
-        <Link
-          href="/"
-          className="mb-8 inline-flex items-center gap-5 text-2xl font-bold text-black md:text-3xl"
-          aria-label="logo"
-        >
-          <Image src="/icons/club-icon.svg" alt="Example Image" width={40} height={40} />
-          Webut
-        </Link>
-
-        <div className="mb-8 w-full text-gray-600 text-sm md:text-base overflow-y-auto h-[60vh] p-4 border border-gray-300 rounded-lg">
+        <div className="mb-8 w-full h-full text-gray-600 text-sm md:text-base overflow-y-auto p-4 border-2 border-gray-700 rounded-lg">
           <ReactMarkdown components={{
-            h1: (props) => <h1 className="text-gray-700 text-2xl font-bold" {...props} />,
-            h2: (props) => <h2 className="text-gray-600 text-xl font-semibold mt-4" {...props} />,
-            h3: (props) => <h3 className="text-gray-500 text-lg font-semibold mt-3" {...props} />,
+            h1: (props) => <h1 className="text-gray-950 text-2xl font-bold" {...props} />,
+            h2: (props) => <h2 className="text-gray-900 text-xl font-semibold mt-4" {...props} />,
+            h3: (props) => <h3 className="text-gray-600 text-lg font-semibold mt-3" {...props} />,
             p: (props) => <p className="text-black mt-2" {...props} />,
             li: (props) => <li className="text-black ml-6 list-disc" {...props} />,
             blockquote: (props) => <blockquote className="bg-gray-100 border-l-4 border-gray-500 text-gray-700 p-4 italic" {...props} />,
@@ -74,7 +65,6 @@ export default function DocumentPage() {
 
         <div className="flex flex-row items-end justify-center gap-5">
           <p className="text-gray-500 mb-4">最終更新: {new Date(data?.updated_at || '').toLocaleString()}</p>
-          <p className="text-gray-500 mb-4">作成日: {new Date(data?.created_at || '').toLocaleString()}</p>
         </div>
       </div>
     </div>
