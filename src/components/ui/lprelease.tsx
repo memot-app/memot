6b966442fdc0185f5a0ed3269ee63edf792b818f
YoneKairi <yone.march@gmail.com>
2025-04-06 16:53:40 +0900

    style:メディアクエリの追加

--- src/components/ui/lprelease.tsx
+++ src/components/ui/lprelease.tsx
@@ -3,15 +3,9 @@ import React from "react"
 
 export default function Lptext(){
     return(
-        <div className="">
-            <div className="relative w-225 h-45 bg-[#F7F4F2] rounded-4xl mx-auto">
-                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[42px] font-semibold">2025初夏  リリース予定</p>
-                
-                <p className="absolute top-[55%] left-1/2 -translate-x-1/2 mt-6 text-sm text-black">※IOS・Android・Webで提供予定</p>
-              
-
-            </div>
-            
+        <div className="flex flex-col items-center justify-center w-[80%] h-45 bg-[#F7F4F2] rounded-4xl mx-auto relative">
+            <p className="text-[clamp(1.5rem,5vw,2.8rem)] font-semibold">2025初夏 リリース予定</p>
+            <p className="text-sm whitespace-nowrap  text-black absolute bottom-6 left-1/2 -translate-x-1/2">※IOS・Android・Webで提供予定</p>
         </div>
     )
 
