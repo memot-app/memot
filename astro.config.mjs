1bb784dcebf6ec5b60ed31cc55debdfe73be53c4
Shvain <takuto3987@gmail.com>
2025-09-03 02:12:43 +0900

    fix：メッセージカードの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { loadEnv } from 'vite';
 const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 export default defineConfig({
-  site: env.DEV ? 'http://localhost:3000' : 'https://webut-web.vercel.app',
+  site: env.DEV ? 'http://localhost:4321' : 'https://lp.memot.app',
   integrations: [
     sitemap(),
   ],
